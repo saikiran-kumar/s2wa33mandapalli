9eb6923ec5ef9609a9c02c7cf3d5607b39821aef
saikiran-kumar <saikirankumar34@gmail.com>
2022-03-30 11:22:33 -0500

    added times

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+var times=0;
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  times=times+1;
+  res.send('User accesses are: '+times);
 });
 
 module.exports = router;
