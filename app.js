f4e9682edbae2efad238d45374f7de3bd2695477
saikiran-kumar <saikirankumar34@gmail.com>
2022-03-30 11:26:29 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/mydata');
+var indexRouter = require('./routes/users');
 var usersRouter = require('./routes/users');
 
 var app = express();
