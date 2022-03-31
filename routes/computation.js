16b19b2545bb2f502e659830ef2aee7a56d55d81
saikiran-kumar <saikirankumar34@gmail.com>
2022-03-30 19:00:45 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -6,7 +6,7 @@ var x=Math.round(Math.random()*100)/100;
 
 router.get('/', function(req, res, next) {
   
-  res.send(`Ceil Applied to:(${x}) is `+Math.ceil(x));
+  res.send(`Ceil Applied to : (${x}) is `+Math.ceil(x));
 });
 
 module.exports = router;
